4d41a75360d46518d5f828ae807cdbac35a5ff81
Yanir Tamir <yanir334@gmail.com>
2021-11-17 18:43:29 +0200

    feat(app): fixed a bunch of badly placed components & graph bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {},
+			country: ''
+		};
+	}
+
+	async componentDidMount() {
+		const fetchedData = await fetchData();
+		this.setState({ data: fetchedData });
+	}
+	async handleCountryChange(country) {
+		const fetchedData = await fetchData(country);
+		this.setState({ data: fetchedData, country: country });
+	}
+	render() {
+		const { data, country } = this.state;
+		return (
+			<div className={styles.container}>
+				<img src="https://i.ibb.co/7QpKsCX/image.png"></img>
+				<Cards data={data} />
+				<CountryPicker onChangeCountry={country => this.handleCountryChange(country)} />
+				<Chart data={data} country={country} />
+			</div>
+		);
+	}
 }
 
 export default App;
